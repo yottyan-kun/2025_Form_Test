bf98a0ca0e23f0a6d12a277ddddca8f78e4feaa1
MSI\81809 <endou.yoshi.1005@gmail.com>
2025-10-24 15:41:44 +0900

    内容変更した

--- Form_Test/Form_Test/Form1.Designer.cs
+++ Form_Test/Form_Test/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.テストボタン = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // テストボタン
+            // 
+            this.テストボタン.Location = new System.Drawing.Point(557, 49);
+            this.テストボタン.Name = "テストボタン";
+            this.テストボタン.Size = new System.Drawing.Size(170, 92);
+            this.テストボタン.TabIndex = 0;
+            this.テストボタン.Text = "テストボタン";
+            this.テストボタン.UseVisualStyleBackColor = true;
+            this.テストボタン.Click += new System.EventHandler(this.テストボタン_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.テストボタン);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button テストボタン;
     }
 }
 
--- Form_Test/Form_Test/Form1.cs
+++ Form_Test/Form_Test/Form1.cs
@@ -15,6 +15,38 @@ namespace Form_Test
         public Form1()
         {
             InitializeComponent();
+
+
+            for (int j = 0; j < 4; j++)
+            {
+
+
+                for (int i = 0; i < 6; i++)
+                {
+                    //インスタンスの生成
+                    TestButton testButton = new TestButton();
+
+                    //ボタンの位置を設定
+                    testButton.Location = new Point(50 * i, 50 * j);
+
+                    //ボタンの大きさを設定
+                    testButton.Size = new Size(50, 50);
+
+                    //ボタン内のテキストを設定
+                    testButton.Text = "あいうえお";
+
+                    //コントロールにボタンを追加
+                    Controls.Add(testButton);
+
+                }
+            }
+        }
+
+     
+
+        private void テストボタン_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("おはようございます");
         }
     }
 }
